0ebd8f78c03a1ab20c51f14b812e917c97191cc2
pascalle <pb.fsu10@gmail.com>
2021-05-12 10:46:31 +0200

    new debiased code with smaller sets

---
+++ pascalle/test_debiased.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 12 10:43:16 2021
+
+@author: bananasacks
+"""
+
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import ot
+import warnings
+import sys
+sys.path.insert(1, '/Users/bananasacks/Desktop/Optimal Transport Internship/Optimal_Transport/pascalle/')
+from debiased_ot_barycenters import sinkhorn_barycenters as sink
+
+
+#rng = np.random.RandomState(42)
+#n = 2
+#index = rng.choice(np.arange(200), size=(n), replace=False)
+#index.sort()
+#print(index)
+
+
+#can add index back here
+noise15 = np.load("./artificial_data.npy")[15, :, :]
+no_noise15 = np.load("./artificial_data_no_noise.npy")[15, :, :]
+noise95 = np.load("./artificial_data.npy")[95, :, :]
+no_noise95 = np.load("./artificial_data_no_noise.npy")[95, :, :]
+
+
+#making dataset smaller
+noise15 = noise15[23:46,23:46]
+no_noise15 = no_noise15[23:46,23:46]
+no_noise95 = no_noise95[16:39,27:50]
+noise95 = noise95[16:39,27:50]
+
+
+P_noise = np.array([noise15,noise95])
+P_nonoise = np.array([no_noise15, no_noise95])
+
+epsilon = 1e-1
+C = ot.dist(noise15, noise95)
+print(C)
+#normalize
+#C /= C.max()
+expC = lambda i: math.exp(-i/epsilon)
+expC = np.vectorize(expC)
+
+K = expC(C)
+print(K.shape)
+
+#noiseP =  torch.Tensor(noise)
+P =  torch.from_numpy(P_nonoise)
+K = torch.from_numpy(K)
+
+sink.barycenter_debiased_2d(P, K)
