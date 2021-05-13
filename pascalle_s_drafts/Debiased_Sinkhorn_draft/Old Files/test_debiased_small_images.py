89f00324944b5a1e39f29e860feb6971eeaa3340
pascalle <pb.fsu10@gmail.com>
2021-05-13 13:39:23 +0200

    debiased code working with 2 images

--- pascalle_s_drafts/Debiased_Sinkhorn_draft/Old Files/test_debiased_small_images.py
+++ pascalle_s_drafts/Debiased_Sinkhorn_draft/Old Files/test_debiased_small_images.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 13 10:00:15 2021
+Created on Wed May 12 10:43:16 2021
 
 @author: bananasacks
 """
@@ -16,39 +16,50 @@ import warnings
 import sys
 import sinkhorn_barycenters as sink
 
-#Loading 2 sample images
-noise15 = abs(np.load("./artificial_data.npy")[15, :, :])
+
+#rng = np.random.RandomState(42)
+#n = 2
+#index = rng.choice(np.arange(200), size=(n), replace=False)
+#index.sort()
+#print(index)
+
+
+#can add index back here
+#working with 2 images noise vs no noise
+noise15 = np.load("./artificial_data.npy")[15, :, :]
 no_noise15 = np.load("./artificial_data_no_noise.npy")[15, :, :]
-noise95 = abs(np.load("./artificial_data.npy")[95, :, :])
+noise95 = np.load("./artificial_data.npy")[95, :, :]
 no_noise95 = np.load("./artificial_data_no_noise.npy")[95, :, :]
+#making dataset smaller
+noise15 = noise15[20:43,20:43]
+no_noise15 = no_noise15[20:43,20:43]
+no_noise95 = no_noise95[19:42,27:50]
+noise95 = noise95[19:42,27:50]
 
-#constants
-width = 50
 epsilon = 1e-1
-
 noise = np.array([noise15,noise95])
 nonoise = np.array([no_noise15, no_noise95])
 
-#formatting function
+
 def debiased_format(imgs, epsilon):
-    P = torch.from_numpy(imgs)
-    
-    grid = torch.arange(width).type(torch.float64)
-    grid /= width
-    M = (grid[:, None] - grid[None, :]) ** 2
-    K = torch.exp(- M / epsilon)
-    #K = torch.from_numpy(K)
-    return P, K
+  C = ot.dist(imgs[0], imgs[1])
+  #normalize
+  C /= C.max()
+  expC = lambda i: math.exp(-i/epsilon)
+  expC = np.vectorize(expC)
+  K = expC(C)
+  P = torch.from_numpy(imgs)
+  K = torch.from_numpy(K)
+  return P, K
 
-#creating variables for debiased algo
 P_nonoise, K_nonoise = debiased_format(nonoise, epsilon)
-P_noise, K_noise = debiased_format(noise, epsilon)
+q_nonoise = sink.barycenter_debiased_2d(P_nonoise, K_nonoise)
+q_nonoise = np.array(q_nonoise)
 
-#running debiased algo
-q_noise = sink.barycenter(P_noise, K_noise, reference="debiased", maxiter = 15000)
-q_nonoise = sink.barycenter(P_nonoise, K_nonoise, reference="debiased", maxiter = 15000)
+P_noise, K_noise = debiased_format(noise, .3)
+q_noise = sink.barycenter_debiased_2d(P_noise, K_noise, maxiter=1000000)
+q_noise = np.array(q_noise)
 
-#plotting results
 plt.figure(1, figsize=(10,10))
 plt.subplot(2,2,1)
 plt.imshow(noise15)
@@ -84,6 +95,6 @@ plt.title("Debiased Barycenter with No Noise")
 plt.axis('off')
 
 
-#print(P_noise[0][14])
-#print(q_noise[0])
-#print(q_nonoise[0])
+print(P_noise[0][14])
+print(q_noise[14])
+print(q_nonoise[14])
