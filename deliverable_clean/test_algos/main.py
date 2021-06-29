c3d59c2f67edb4551b6895d232c20fe439de57c7
Richard S <serrano.richard@protonmail.com>
2021-06-29 16:14:21 +0200

    modification on main and run_main scripts

--- deliverable_clean/test_algos/main.py
+++ deliverable_clean/test_algos/main.py
@@ -1,30 +1,31 @@
 #! /usr/bin/env python3
 
-# from debiased_sink_bary import debiased_sink_bary
-# from kbcm_bary import kbcm_bary
-# from entropic_reg_bary import entropic_reg_bary
-# from tlp_bary import tlp_bary
+from debiased_sink_bary import debiased_sink_bary
+from kbcm_bary import kbcm_bary
+from entropic_reg_bary import entropic_reg_bary
+from tlp_bary import tlp_bary
 from multiprocessing import Process, Queue
 import sys
 from os import listdir
 from os.path import join,isfile
 
-def debiased_sink_bary(**kwars):
-    print("debiased_sink_bary")
-    for k,v in kwars.items():
-        print(k,":",v)
-def kbcm_bary(**kwars):
-    print("kbcm_bary")
-    for k,v in kwars.items():
-        print(k,":",v)
-def entropic_reg_bary(**kwars):
-    print("entropic_reg_bary")
-    for k,v in kwars.items():
-        print(k,":",v)
-def tlp_bary(**kwars):
-    print("tlp_bary")
-    for k,v in kwars.items():
-        print(k,":",v)
+
+# def debiased_sink_bary(**kwars):
+#     print("debiased_sink_bary")
+#     for k,v in kwars.items():
+#         print(k,":",v)
+# def kbcm_bary(**kwars):
+#     print("kbcm_bary")
+#     for k,v in kwars.items():
+#         print(k,":",v)
+# def entropic_reg_bary(**kwars):
+#     print("entropic_reg_bary")
+#     for k,v in kwars.items():
+#         print(k,":",v)
+# def tlp_bary(**kwars):
+#     print("tlp_bary")
+#     for k,v in kwars.items():
+#         print(k,":",v)
 
 
 Q = Queue()
@@ -99,7 +100,6 @@ def run_kbcm(reg, max_iter, c, intensity) :
     Q.put(' '.join(logs))
 
 
-
 def run_tlp(reg, eta, intensity):
     logs=[]
     logs.append(f"\nregression = {reg}, eta = {eta}, intensity = {intensity}"+"\n")
@@ -116,113 +116,135 @@ def run_tlp(reg, eta, intensity):
 
 
 
-def main():
+def main(algo = None):
     dbs_files = get_files("./results/debiased_sink_bary")
     entr_files = get_files("./results/entropic_reg_bary")
     kbcm_files = get_files("./results/kbcm_bary")
     tlp_files = get_files("./results/tlp_bary")
 
-    ############
-    # debiased #
-    ############
-    processes = []
-    Q.put("\n\n\n\nRunning DSB"+"\n")
+    algo_to_run = [0 for i in range(4)]
 
-    params = [#(.001,    int(1e6) ),
-              #(.005,    int(1e6) ),
-              (.01,     int(1e6) ),
-              (.01,     int(1e8) ),
-              (.05,     int(1e7) ),
-              (.2,      2500     ),
-              (.2,      3000     ),
-              (.2,      4000     ),
-              (.5,      500      ),
-              (.5,      750      ),
-              (.5,      1000     ),
-              (.7,      100      ),
-              (.7,      750      ),
-              (.7,      1000     )]
-
-    for epsilon,max_iter in params :
-        if not check_in_files(dbs_files,eps = epsilon, iter = max_iter) :
-            processes.append( Process(target=run_dsb, args=(epsilon, max_iter,)) )
-            processes[-1].start()
-
-    for proc in processes :
-        proc.join()
-    Q.put("\n\n\n\n"+"-"*50+"\n")
-
-    with open("./logs.txt", "w") as file :
-        while not Q.empty() :
-            file.write(Q.get())
+    if algo == None :
+        algo_to_run = [1 for i in range(4)]
+    else :
+        conv = {"dsb"    : 0,
+                "0"      : 0,
+                "ent"    : 1,
+                "1"      : 1,
+                "kbcm"   : 2,
+                "2"      : 2,
+                "tlp"    : 3,
+                "3"      : 3}
+        algo_to_run[conv[algo]] = 1
 
 
+    if algo_to_run[0] :
+        ############
+        # debiased #
+        ############
+        processes = []
+        Q.put("\n\n\n\nRunning DSB"+"\n")
 
-    ############
-    # entropic #
-    ############
-    processes = []
-    Q.put("\n\n\n\nRunning ERB"+"\n")
-    for reg in [0.075, 0.4, 1, 4] :
-        if not check_in_files(entr_files, reg = reg) :
-            processes.append( Process(target=run_entropic, args=(reg,)) )
-            processes[-1].start()
-    
-    for proc in processes :
-        proc.join()
-    Q.put("\n\n\n\n"+"-"*50+"\n")
+        params = [(.001,    int(1e6) ),
+                  (.005,    int(1e6) ),
+                  (.01,     int(1e6) ),
+                  (.01,     int(1e8) ),
+                  (.05,     int(1e7) ),
+                  (.2,      2500     ),
+                  (.2,      3000     ),
+                  (.2,      4000     ),
+                  (.5,      500      ),
+                  (.5,      750      ),
+                  (.5,      1000     ),
+                  (.7,      100      ),
+                  (.7,      750      ),
+                  (.7,      1000     )]
 
+        for epsilon,max_iter in params :
+            if not check_in_files(dbs_files,eps = epsilon, iter = max_iter) :
+                processes.append( Process(target=run_dsb, args=(epsilon, max_iter,)) )
+                processes[-1].start()
 
-    with open("./logs.txt", "a") as file :
-        while not Q.empty() :
-            file.write(Q.get())
+        for proc in processes :
+            proc.join()
+        Q.put("\n\n\n\n"+"-"*50+"\n")
 
-
-
-    ########
-    # kbcm #
-    ########
-    processes = []
-    Q.put("\n\n\n\nRunning KBCM"+"\n")
-    for reg in [.001, .01, .05, .1, .25, .4, .5, .6, .9] :
-        for max_iter in [100,500] :
-            for c in [-.5] :
-                for intensity in ["minmax"] :
-                    if not check_in_files(kbcm_files, reg = reg, iters = max_iter) :
-                        processes.append( Process(target=run_kbcm, args=(reg, max_iter,c,intensity)) )
-                        processes[-1].start()
-        
-    for proc in processes :
-        proc.join()
-    Q.put("\n\n\n\n"+"-"*50+"\n")
-
-    with open("./logs.txt", "a") as file :
-        while not Q.empty() :
-            file.write(Q.get())
+        with open("./logs0.txt", "w") as file :
+            while not Q.empty() :
+                file.write(Q.get())
 
 
-    #######
-    # TLp #
-    #######
-    processes = []
-    Q.put("\n\n\n\nRunning TLp"+"\n")
-    for reg in [.001, .01, .05, .1, .5, .9] :
-        for eta in  [.001, .1, .05, .7] :
-            for intensity in ["minmax", "zeroone"] :
-                if not check_in_files(tlp_files, reg = reg, eta = eta, intensity = intensity) :
-                    processes.append( Process(target=run_tlp, args=(reg, eta, intensity,)) )
+    if algo_to_run[1] :
+        ############
+        # entropic #
+        ############
+        processes = []
+        Q.put("\n\n\n\nRunning ERB"+"\n")
+        for reg in [0.075, 0.4, 1, 4] :
+            for metric in ["sqeuclidean", "cityblock"] :
+                if not check_in_files(entr_files, reg = reg) :
+                    processes.append( Process(target=run_entropic, args=(reg,metric)) )
                     processes[-1].start()
-        
-    for proc in processes :
-        proc.join()
 
-    with open("./logs.txt", "a") as file :
-        while not Q.empty() :
-            file.write(Q.get())
+        for proc in processes :
+            proc.join()
+        Q.put("\n\n\n\n"+"-"*50+"\n")
+
+
+        with open("./logs1.txt", "a") as file :
+            while not Q.empty() :
+                file.write(Q.get())
+
+
+    if algo_to_run[2] :
+        ########
+        # kbcm #
+        ########
+        processes = []
+        Q.put("\n\n\n\nRunning KBCM"+"\n")
+        for reg in [.001, .01, .05, .1, .25, .4, .5, .6, .9] :
+            for max_iter in [100,500] :
+                for c in [-.5] :
+                    for intensity in ["minmax"] :
+                        if not check_in_files(kbcm_files, reg = reg, iters = max_iter) :
+                            processes.append( Process(target=run_kbcm, args=(reg, max_iter,c,intensity)) )
+                            processes[-1].start()
+            
+        for proc in processes :
+            proc.join()
+        Q.put("\n\n\n\n"+"-"*50+"\n")
+
+        with open("./logs2.txt", "a") as file :
+            while not Q.empty() :
+                file.write(Q.get())
+
+
+    if algo_to_run[3] :
+        #######
+        # TLp #
+        #######
+        processes = []
+        Q.put("\n\n\n\nRunning TLp"+"\n")
+        for reg in [.001, .01, .05, .1, .5, .9] :
+            for eta in  [.001, .1, .05, .7] :
+                for intensity in ["minmax", "zeroone"] :
+                    if not check_in_files(tlp_files, reg = reg, eta = eta, intensity = intensity) :
+                        processes.append( Process(target=run_tlp, args=(reg, eta, intensity,)) )
+                        processes[-1].start()
+            
+        for proc in processes :
+            proc.join()
+
+        with open("./logs3.txt", "a") as file :
+            while not Q.empty() :
+                file.write(Q.get())
 
 
 
 
 
 if __name__=="__main__"   :
-    main()
+    if len(sys.argv) == 1:
+        main()
+    else :
+        main(algo = sys.argv[1])
--- deliverable_clean/test_algos/run_main.sh
+++ deliverable_clean/test_algos/run_main.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 #SBATCH -p GPU-DEPINFO
-#SBATCH -n 1
+#SBATCH -n 2 
 #SBATCH -N 1
-#SBATCH --ntasks-per-node 1
+#SBATCH --ntasks-per-node 2
 #SBATCH -c 1
-#SBATCH --mem=12G
+#SBATCH --mem=50G
 #SBATCH --gres=gpu:1
-#SBATCH -t 4:00:00
-#SBATCH -o main.out
-#SBATCH -e main.err
-#SBATCH -J MAIN_ALGS
+#SBATCH -t 64:00:00
+#SBATCH -o TLP.out
+#SBATCH -e TLP.err
+#SBATCH -J TLP
 
 
 source /home_expes/tools/python/Python-3.8.7_gpu/bin/activate
 
-srun --exclusive python3 ~/Optimal_Transport/deliverable_clean/test_algos/main.py
+srun --exclusive python3 ~/Optimal_Transport/deliverable_clean/test_algos/main.py 3
