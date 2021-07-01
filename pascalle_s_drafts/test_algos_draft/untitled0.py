812940c155ef8b6803b2ac3de180a6b8698343c5
pascalle <pb.fsu10@gmail.com>
2021-07-01 10:01:03 +0200

    most recent debiased sinkhorn algo

--- pascalle_s_drafts/test_algos_draft/untitled0.py
+++ pascalle_s_drafts/test_algos_draft/untitled0.py
@@ -3,36 +3,149 @@
 """
 @author: bananasacks
 """
+#plot 9 rows by 5 columns
+#rows are the diff params and cols are the noise levels
 
-
-import numpy as np
 import os
-from os.path import isfile, join
-os.chdir("/Users/bananasacks/Desktop/Optimal Transport Internship/Optimal_Transport/pascalle_s_drafts/test_algos_draft")
+os.chdir("/Users/bananasacks/Desktop/Optimal Transport Internship/Optimal_Transport/deliverable_clean/test_algos")
+
 import matplotlib.pyplot as plt
+import numpy as np
+import torch
 
 
-def get_files():
-    onlyfiles = [f for f in os.listdir("./results/kbcm_bary") if isfile(join("./results/kbcm_bary", f))]
-    onlyfiles = [file for file in onlyfiles if file[-4:] == ".npy"]
-    #onlyfiles.sort()
-    for file in onlyfiles:
-        yield file
+def findmax(algo, reg, second_param):
+    vmax=[]
+    vmin=[]
+    for r in reg:
+        for p in second_param: 
+            if algo == "kbcm":
+                title = "./results/kbcm_bary/bary_noiselvl_"
+                params = "_reg_"+str(r)+"_c_"+str(p)+"_iters_100_intensity_maxmin"
+            elif algo == "tlp":
+                title = "./results/tlp_bary/bary_noiselvl_"
+                params = "_reg_"+str(r)+"_eta_"+str(p)+"_outer-inner_10-100_intensity_minmax"
+            elif algo == "sink":
+                title = "./results/debiased_sink_bary/bary_noiselvl_"
+                params = "_eps_"+str(r)+"_iter_"+str(p)+"_intensity_minmax"
+            else:
+                print("something went wrong")
+                
+            path = title + "0.200" + params + ".npy"
+            if os.path.isfile(path):                
+                noise_lvls = ["0.000", "0.100", "0.200", "0.500", "1.000"]
+                for n in noise_lvls: 
+                    barys = np.load(title + n + params + ".npy")
+                    nanmin = np.nanmin(barys)
+                    nanmax = np.nanmax(barys)
+            #Finding max and min intensities for consistent plotting
+                    #Finding the Min intensity with NAN handler
+                    #print(vmin)
+                    #print(nanmin)
+                    if vmin == []:
+                        if np.isnan(nanmin) == True:
+                            vmin = []
+                        else:
+                            vmin = nanmin
+                    ##If NAN, do nothing
+                    ##If min(bary) > vmin, do nothing
+                        #print(vmin)
+                    else:
+                        if np.isnan(nanmin) == False:
+                            barymin = nanmin
+                            if  barymin < vmin:
+                                vmin = barymin 
+                        #print(vmin)
+                    if np.isnan(vmin):
+                        vmin = 0
+                    #print(vmin)
+                    #Finding the Max intensity with NAN handler
+                    if vmax == []:
+                        if np.isnan(nanmax) == True:
+                            vmax = []
+                        else:
+                            vmax = nanmax
+                    ##If NAN, do nothing
+                    ##If max(bary) < vmax, do nothing
+                    else:
+                        if np.isnan(nanmax) == False:
+                            barymax = nanmax
+                            if  barymax > vmax:
+                                vmax = barymax     
+                    if np.isnan(vmax):
+                        vmax = 1
+    return vmax
+                
+
+def bigplotkt(algo, reg, second_param, vmax):  
+    plt.figure(1, figsize=(15, 10)) 
+    #fig, ax = plt.subplots(10,5)
+    k = 1 
+    
+    for r in reg:
+        for p in second_param: 
+            if algo == "kbcm":
+                title = "./results/kbcm_bary/bary_noiselvl_"
+                params = "_reg_"+str(r)+"_c_"+str(p)+"_iters_100_intensity_maxmin"
+            elif algo == "tlp":
+                title = "./results/tlp_bary/bary_noiselvl_"
+                params = "_reg_"+str(r)+"_eta_"+str(p)+"_outer-inner_10-100_intensity_minmax"
+            elif algo == "sink":
+                title = "./results/debiased_sink_bary/bary_noiselvl_"
+                params = "_eps_"+str(r)+"_iter_"+str(p)+"_intensity_minmax"
+            else:
+                print("something went wrong")
+            
+                          
+            path = title + "0.000" + params + ".npy"
+            if os.path.isfile(path):
+                print("_reg_"+str(r)+"_c_"+str(p))  
+                
+                noise_lvls = ["0.000", "0.100", "0.200", "0.500", "1.000"]
+                
+                #fig.suptitle(title[10:-15] + params)
+                for n in noise_lvls: 
+                    barys = np.load(title + n + params + ".npy")
+                    #print(n)
+                    plt.subplot(20, 5, k)
+                    k += 1
+                    plt.axis("off")
+                    plt.imshow(barys, vmin=0, vmax=vmax)
+                    #plt.ylabel("reg_"+str(r)+"_c_"+str(p))
+                    #plt.xlabel()
+                
+            plt.suptitle(title[10:-15] + "     max_amplitude: "+ str(round(vmax,5)))
+    
+                #plt.savefig("results/report_plots/"+title[10:-15] + params + ".png")
+    
+    plt.show()
+                
+                
+                
+                
+"""
+algo = "kbcm"
+reg = [0.1, 0.01, 0.001, 0.4, 0.5, 0.05, 0.6, 0.9, 0.25, 0.75, 1, 4]
+c = [-0.5, -0.7]
+vmax = findmax(algo, reg, c)
+print(vmax)
+bigplotkt(algo, reg, c, vmax)
     
 
-#add def here
-        
-files = get_files()       
-#if plot:
-plt.figure(1, figsize=(15, 10))
-k = 1
+algo = "tlp"
+reg = [0.4, 0.05, 0.75, 1, 4]
+eta = [0.1, 0.001, 0.05, 0.7]
+vmax = findmax(algo, reg, eta)
+print(vmax)
+bigplotkt(algo, reg, eta, vmax)  
+"""    
 
-for file in files:
-    bary = np.load("./results/kbcm_bary/" + file, allow_pickle = True)
-    title = file[:-4]
-    print(title)
-
-
-#need to get a unique list of parameter groupings
-#then I want to plot all 6 charts (6 noise levels) in that grouping
-#get a list of regs with c as second column
+algo = "sink"
+reg = [0.1, 0.001, 0.2, 0.5, 0.005, 0.6, 0.7]
+iters = [100,300,500,1000000, 2500, 3000, 4000, 1000]
+vmax = findmax(algo, reg, iters)
+print(vmax)
+bigplotkt(algo, reg, iters, vmax)  
+               
+    
+    
--- pascalle_s_drafts/test_algos_draft/untitled2.py
+++ pascalle_s_drafts/test_algos_draft/untitled2.py
@@ -1,47 +1,136 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed May 26 22:50:25 2021
-
 @author: bananasacks
 """
+#plot 9 rows by 5 columns
+#rows are the diff params and cols are the noise levels
 
-import numpy as np
 import os
-os.chdir("/Users/bananasacks/Desktop/Optimal Transport Internship/Optimal_Transport/pascalle_s_drafts/test_algos_draft")
+os.chdir("/Users/bananasacks/Desktop/Optimal Transport Internship/Optimal_Transport/deliverable_clean/test_algos")
+
 import matplotlib.pyplot as plt
-#from load_data import load_data
+import numpy as np
+import torch
 
 
+def findmax(algo, reg, second_param):
+    vmax=[]
+    vmin=[]
+    for r in reg:
+        for p in second_param: 
+            title = "./results/debiased_sink_bary/bary_noiselvl_"
+            params = "_eps_"+str(r)+"_iter_"+str(p)+"_intensity_minmax"
+                
+            path = title + "0.000" + params + ".npy"
+            if os.path.isfile(path):                
+                noise_lvls = ["0.000", "0.100", "0.200", "0.500", "1.000"]
+                for n in noise_lvls: 
+                    barys = np.load(title + n + params + ".npy")
+                    nanmin = np.nanmin(barys)
+                    nanmax = np.nanmax(barys)
+            #Finding max and min intensities for consistent plotting
+                    #Finding the Min intensity with NAN handler
+                    #print(vmin)
+                    #print(nanmin)
+                    if vmin == []:
+                        if np.isnan(nanmin) == True:
+                            vmin = []
+                        else:
+                            vmin = nanmin
+                    ##If NAN, do nothing
+                    ##If min(bary) > vmin, do nothing
+                        #print(vmin)
+                    else:
+                        if np.isnan(nanmin) == False:
+                            barymin = nanmin
+                            if  barymin < vmin:
+                                vmin = barymin 
+                        #print(vmin)
+                    if np.isnan(vmin):
+                        vmin = 0
+                    #print(vmin)
+                    #Finding the Max intensity with NAN handler
+                    if vmax == []:
+                        if np.isnan(nanmax) == True:
+                            vmax = []
+                        else:
+                            vmax = nanmax
+                    ##If NAN, do nothing
+                    ##If max(bary) < vmax, do nothing
+                    else:
+                        if np.isnan(nanmax) == False:
+                            barymax = nanmax
+                            if  barymax > vmax:
+                                vmax = barymax     
+                    if np.isnan(vmax):
+                        vmax = 1
+    return vmax
+                
 
-
-#bary_lvl_0.000_mean_0.000_eps_0.7.npy
-#bary_lvl_0.200_mean_0.000_eps_0.7.npy
-#bary_lvl_0.050_mean_0.000_eps_0.7.npy
-
-
-lvl = ["0.000", "0.050", "0.100", "0.200", "0.500", "0.100"]
-eps = ["0.5"]
+def bigplotkt(algo, reg, second_param, vmax):  
+    plt.figure(1, figsize=(15, 10)) 
+    #fig, ax = plt.subplots(10,5)
+    k = 1 
     
-
-bary0 = np.load("results/debiased_sink_bary/bary_lvl_0.000_mean_0.000_eps_"+eps[0]+".npy")#, allow_pickle=True)
+    for r in reg:
+        for p in second_param: 
+            if algo == "kbcm":
+                title = "./results/kbcm_bary/bary_noiselvl_"
+                params = "_reg_"+str(r)+"_c_"+str(p)+"_iters_100_intensity_maxmin"
+            elif algo == "tlp":
+                title = "./results/tlp_bary/bary_noiselvl_"
+                params = "_reg_"+str(r)+"_eta_"+str(p)+"_outer-inner_10-100_intensity_minmax"
+            else:
+                print("something went wrong")
+            
+                          
+            path = title + "0.000" + params + ".npy"
+            if os.path.isfile(path):
+                print("_reg_"+str(r)+"_c_"+str(p))  
+                
+                noise_lvls = ["0.000", "0.100", "0.200", "0.500", "1.000"]
+                
+                #fig.suptitle(title[10:-15] + params)
+                for n in noise_lvls: 
+                    barys = np.load(title + n + params + ".npy")
+                    #print(n)
+                    plt.subplot(11, 5, k)
+                    k += 1
+                    plt.axis("off")
+                    plt.imshow(barys, vmin=0, vmax=vmax)
+                    #plt.ylabel("reg_"+str(r)+"_c_"+str(p))
+                    #plt.xlabel()
+                
+            plt.suptitle(title[10:-15] + "     max_amplitude: "+ str(round(vmax,5)))
     
-print(bary0.max())
-print(bary0.min())
-
-bary2 = np.load("results/debiased_sink_bary/bary_lvl_0.500_mean_0.000_eps_"+eps[0]+".npy")#, allow_pickle=True)
+                #plt.savefig("results/report_plots/"+title[10:-15] + params + ".png")
     
-print(bary2.max())
-print(bary2.min())
+    plt.show()
+                
+                
+                
+                
+
+algo = "kbcm"
+reg = [0.1, 0.01, 0.001, 0.4, 0.5, 0.05, 0.6, 0.9, 0.25, 0.75, 1, 4]
+c = [-0.5, -0.7]
+vmax = findmax(algo, reg, c)
+print(vmax)
+bigplotkt(algo, reg, c, vmax)
     
-    #plt.imshow(bary, vmin=0, vmax=1)
-
-#k=1
-#plt.subplot(2, 3, k)
-#plt.title(title[:len(title)//2]+"\n"+title[len(title)//2:])
-#plt.imshow(bary, vmin=0, vmax=1)
-#k += 1
-
-        
-
-        
+"""
+algo = "tlp"
+reg = [0.4, 0.05, 0.75, 1, 4]
+eta = [0.1, 0.001, 0.05, 0.7]
+vmax = findmax(algo, reg, eta)
+print(vmax)
+bigplotkt(algo, reg, eta, vmax)  
+"""    
+"""
+            elif algo == "sink":
+                title = "./results/debiased_sink_bary/bary_noiselvl_"
+                params = "_eps_"+str(r)+"_iter_"+str(p)+"_intensity_minmax"
+"""                
+    
+    
